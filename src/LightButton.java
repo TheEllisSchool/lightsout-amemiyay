7d83f405ae9d7af740a19e8e751577ced3395203
yumikaamemiya <yumikaamemiya@172.16.21.214>
2018-03-02 12:24:22 -0500

    Complete

--- src/LightButton.java
+++ src/LightButton.java
@@ -1,14 +1,20 @@
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
 
 public class LightButton extends JButton{
 	//attributes
-	private static int SIZE = 50;
-	private boolean isOn = false;
+	private static int SIZE = 100;
+	boolean isOn = false;
+	private int row = 0;
+	private int col = 0;
 	
-	public LightButton() {
-		isOn = false;
+	public LightButton(int r, int c) {
+		row = r;
+		col = c;
+		
+		setBackground(Color.black);
 		
 		Dimension size = new Dimension(SIZE, SIZE);
 		setPreferredSize(size);
@@ -18,15 +24,27 @@ public class LightButton extends JButton{
 		return isOn;
 	}
 	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
 	public void reset() {
 		isOn = false;
+		setBackground(Color.black);
 	}
 	
 	public void toggle() {
-		if (isOn = true) {
+		if (isOn == true) {
 			isOn = false;
+			setBackground(Color.black);
 		} else {
 			isOn = true;
+			setBackground(Color.white);
 		}
 	}
+	
 }
--- src/LightsOut.java
+++ src/LightsOut.java
@@ -2,9 +2,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -16,7 +21,7 @@ public class LightsOut extends JFrame {
 	public LightsOut() {
 		initGUI();
 		
-		setTitle("Mine Duster");
+		setTitle("Framed!");
 		setSize(700, 200); 
 		setResizable(false);
 		pack();
@@ -36,19 +41,103 @@ public class LightsOut extends JFrame {
 		titleLabel.setFont(titleFont);
 		titleLabel.setForeground(Color.black);
 		titlePanel.add(titleLabel);
+		
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(GRIDSIZE, GRIDSIZE));
+		add(centerPanel, BorderLayout.CENTER);
+		for (int r = 0; r < GRIDSIZE; r++) {
+			for (int c = 0; c < GRIDSIZE; c++) {
+				lightBoard[r][c] = new LightButton(r, c);
+				lightBoard[r][c].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						LightButton button = (LightButton) e.getSource();
+						int row = button.getRow();
+						int col = button.getCol();
+						buttonClicked(row, col);
+					}
+				});
+				centerPanel.add(lightBoard[r][c]);
+			}
+		}
+		boardSetUp();
 	}
 	
 	public void boardSetUp() {
-		//start with them on, then do random # of "button clicked"
+		//start with them framed by black
+		for (int r = 0; r < GRIDSIZE; r++) {
+			for (int c = 0; c < GRIDSIZE; c++) {
+				lightBoard[r][c].reset();
+			}
+		}
+		lightBoard[1][1].toggle();
+		//random # of "button clicked"
+		randomClicks();
 	}
-	
+
 	public void buttonClicked(int r, int c) {
-		//toggles that button and neighbors
-		//check for win --> all outside on
+		//toggles that button
+		lightBoard[r][c].toggle();
+		//toggle neighbors
+		if (check(r-1, c)) {
+			lightBoard[r-1][c].toggle();
+		}
+		if (check(r, c+1)) {
+			lightBoard[r][c+1].toggle();
+		}
+		if (check(r+1, c)) {
+			lightBoard[r+1][c].toggle();
+		}
+		if (check(r, c-1)) {
+			lightBoard[r][c-1].toggle();
+		}
+		
+		if (lightBoard[1][1].getIsOn() && !lightBoard[0][0].getIsOn() && !lightBoard[0][1].getIsOn() 
+				&& !lightBoard[0][2].getIsOn() && !lightBoard[1][0].getIsOn() && !lightBoard[1][2].getIsOn()
+				&& !lightBoard[2][0].getIsOn() && !lightBoard[2][1].getIsOn() && !lightBoard[2][2].getIsOn()) {
+			String message = "You won!";
+			promptForNewGame(message);
+		}
 	}
 	
-	public void promptForNewGame() {
-		boardSetUp();
+	private void randomClicks() {
+		Random rand = new Random();
+		int pickRow;
+		int pickCol;
+		for (int i = 0; i < 10; i++) {
+			pickRow = rand.nextInt(GRIDSIZE);
+			pickCol = rand.nextInt(GRIDSIZE);
+			lightBoard[pickRow][pickCol].toggle();
+			if (check(pickRow-1, pickCol)) {
+				lightBoard[pickRow-1][pickCol].toggle();
+			}
+			if (check(pickRow, pickCol+1)) {
+				lightBoard[pickRow][pickCol+1].toggle();
+			}
+			if (check(pickRow+1, pickCol)) {
+				lightBoard[pickRow+1][pickCol].toggle();
+			}
+			if (check(pickRow, pickCol-1)) {
+				lightBoard[pickRow][pickCol-1].toggle();
+			}
+		}
+	}
+	
+	private boolean check(int r, int c) {
+		if (r > -1 && r < GRIDSIZE && c > -1 && c < GRIDSIZE) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void promptForNewGame(String message) {
+		int option = JOptionPane.showConfirmDialog(this, message, "Play Again?", JOptionPane.YES_NO_OPTION);
+		if (option == JOptionPane.YES_OPTION) {
+			boardSetUp();
+		} else {
+			System.exit(0);
+		}
 	}
 
 	public static void main(String[] args) {
